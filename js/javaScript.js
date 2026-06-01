921cd60036c792b1252e6f03ca1d3f7175c60012
VitorTrevizani <vitoralextrevizani6@gmail.com>
2026-06-01 08:11:50 -0300

    menu responsivo desenvolvido

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,6 +1,12 @@
 const body = document.getElementById("body")
 const botao = document.getElementById("botao")
+const menuIcon = document.getElementById("menu-icon")
+const menu = document.querySelector("nav > ul")
 
 botao.addEventListener("click", () => {
     body.classList.toggle("dark")
+})
+
+menuIcon.addEventListener("click", () => {
+    menu.classList.toggle("menu-oculto")
 })
